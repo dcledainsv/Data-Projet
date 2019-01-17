3d08b3c921b15d966ba2957858061f2631801d1f
GEN-012 <gen@GEN-012>
2019-01-17 13:03:46 +0100

    nouvelle page menu-day

--- php/menu-day.php
+++ php/menu-day.php
@@ -1,21 +1,69 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 	<head>
-		<meta charset="UTF-8">
-	    <link rel="stylesheet" href="../css/style.css">
-	    <link rel="stylesheet" href="../css/menu-day.css">
-	    <title>Menu du jour</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="../css/style.css">
+        <link rel="stylesheet" href="../css/menu-day.css">
+		<title>Accueil</title>
+		<?php 
+    	 
+    	?>
 	</head>
 
 	<body>
 		<header>
-			<?php include("../include/header.php"); ?>
+			<?php 
+			include("../include/header.php");
+			/* Liaison BDD */
+			// include('../request/log-bdd.php');
+			/* Select table */
+			// include('../request/categorie_produit.php');
+			?>
 		</header>
-
+		
 		<section>
-			<h1>Composition du menu de la journée</h1>
+            <h1>Composition du menu de la journee</h1>
+
+            <div class="menu" id="tab_1">
+                <div class="aliment"><img class="image" src="https://static.openfoodfacts.org/images/products/376/004/979/4298/front_fr.77.200.jpg" alt="pain"> </div>
+                
+                <div class="taux">
+                    <div class="pourcentage">0%</div>
+                    <div id="glucides">Glucides</div>
+                </div>
+                <div class="taux">
+                    <div class="pourcentage">0%</div>
+                    <div id="lipides">Lipides</div>
+                </div>
+                <div class="taux">
+                    <div class="pourcentage">0%</div>
+                    <div id="proteines">Protéines</div>
+                </div>
+                <div class="taux">
+                    <div class="pourcentage">0</div>
+                    <div id="calories">Calories</div>
+                </div>
+            </div>
+            
+            <div id="produits">
+				<form method="post" action="#">
+					<div id="listProduit"> <!-- Bloc contenant les produits créés par la fonction "menu-day.js" -->
+					</div>
+
+					<div id="addProduit"> + </div> <!-- Ajouter un produit -->
+
+					<input type="text" name="" placeholder="Nom"> 
+					<input type="submit" name="" value="Enregistrer">
+				</form>
+			</div>
+
+			
 		</section>
 
+		<footer>
+			<?php include("../include/footer.php") ?>
+		</footer>
 
+		<script type="text/javascript" src="../js/menu-day.js"></script>
 	</body>
 </html>
