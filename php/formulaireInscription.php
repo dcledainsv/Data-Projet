df9068a8c7262d9e1ba40f808bb7cc834034fa5f
GEN-004 <gen-004@gen004-HP-ProBook-450-G5>
2019-01-18 16:57:04 +0100

    form verif

--- php/formulaireInscription.php
+++ php/formulaireInscription.php
@@ -15,19 +15,20 @@
 		
 		<section>
             <h2>Informations personnelles</h2>
-                <div class="haut">
-                    <form method="post" action="#">  
+                <form method="post" action="#"> 
+                    <div class="haut">
                         <input type="text" class="input" name="pseudo" value="<?php echo $pseudo;?>" placeholder="Pseudo">
-                        <input type="text" class="input" name="motDePasse" value="<?php echo $motDePasse;?>"placeholder="Mot de passe">
-                        <input type="text" class="input" name="confMotDePasse" value="<?php echo $confMotDePasse;?>"placeholder="Confirmez votre mot de passe">
+                        <span class="error"><?php echo $message;?></span>
+                        <input type="password" class="input" name="motDePasse" value="<?php echo $motDePasse;?>"placeholder="Mot de passe">
+                        <input type="password" class="input" name="confMotDePasse" value="<?php echo $confMotDePasse;?>"placeholder="Confirmez votre mot de passe">
                         <input type="mail" class="input" name="email" value="<?php echo $email;?>"placeholder="Email">
                         <input type="text" class="input" name="nom" value="<?php echo $nom;?>"placeholder="Nom">
                         <input type="text" class="input" name="prenom" value="<?php echo $prenom;?>"placeholder="Prénom">
-                </div>
-                <div class="texte">
-                Pour le calcule de votre ........ il nous faut ces informations complétmentaires
-                </div>
-                <div class="bas">
+                    </div>
+                    <div class="texte">
+                        Pour le calcule de votre ........ il nous faut ces informations complétmentaires
+                    </div>
+                    <div class="bas">
                         <label for="dateDeNaissance">Date de naissance</label>
                         <input type="date" class="input" id="dateDeNaissance" name="dateDeNaissance" value="<?php echo $dateDeNaissance;?>"placeholder="Date de naissance">
                         <label for="femme">Femme</label>
@@ -37,162 +38,168 @@
                         <input type="number"  class="input" name="taille" value="<?php echo $taille;?>"placeholder="Taille (cm)">
                         <input type="number"  class="input" name="poids" value="<?php echo $poids;?>"placeholder="Poids (Kg)">
                         <select class="input"name="activite" >
-                            <option value="Activité"  >Sélectionnez une activité</option>
-                            <option value="Sédentaire" >Sédentaire</option>
-                            <option value="Légèrement actif" >Légèrement actif</option>
-                            <option value="Actif" >Actif</option>
-                            <option value="Sportif" >Sportif</option>
+                            <option >Sélectionnez une activité</option>
+                            <option >Sédentaire</option>
+                            <option >Légèrement actif</option>
+                            <option >Actif</option>
+                            <option >Sportif</option>
                         </select>                
-                </div>       
+                    </div>       
                         <input type="submit" class="input" name="jeMinscris" value="Je m'inscris"> 
-                    </form>
+                </form>
         </section>
         
         <?php
-// définir les variables avec des valeurs vides
-$pseudoErr = $motDePasseErr = $confMotDePasseErr = $emailErr = $nomErr = $prenomErr = $dateDeNaissanceErr = $sexeErr = $tailleErr = $poidsErr = $activite = "";
-$pseudo = $motDePasse = $confMotDePasse = $email = $nom = $prenom = $dateDeNaissance = $sexe = $taille = $poids = $activite = "";
+            // définir les variables avec des valeurs vides
+            $pseudoErr = $motDePasseErr = $confMotDePasseErr = $emailErr = $nomErr = $prenomErr = $dateDeNaissanceErr = $sexeErr = $tailleErr = $poidsErr = $activite = "";
+            $pseudo = $motDePasse = $confMotDePasse = $email = $nom = $prenom = $dateDeNaissance = $sexe = $taille = $poids = $activite = "";
 
-if (isset($_POST["jeMinscris"])) {
-  if (empty($_POST["pseudo"])) {
-    $pseudoErr = "Le pseudo est requis";
-  } else {
-    $pseudo = test_input($_POST["pseudo"]);
-    // vérif si le nom ne contient que des lettres et des espaces
-    if (!preg_match("/^[a-zA-Z ]*$/",$pseudo)) {
-      $pseudoErr = "Seules les lettres et les espaces sont autorisés"; 
-    }
-  }
-  if (empty($_POST["motDePasse"])) {
-    $motDePasseErr = "Le mot de passe est requis";
-  } else {
-    $motDePasse = test_input($_POST["motDePasse"]);
-    // vérif si le mot de passe contient majuscule, minuscule, chiffre, et caractères spéciaux
-    if (!preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W)/", $motDePasse)) {
-      $motDePasseErr = "Mot de passe non conforme, doit contenir une majuscule, minuscule, chiffre, et caractères spéciaux"; 
-    }
-  }
-  
-  if (empty($_POST["confMotDePasse"])) {
-    $confMotDePasseErr = "La confirmation du mot de passe est requis";
-  } else {
-    $confMotDePasse= test_input($_POST["confMotDePasse"]);
-    // vérif si mot de passe identique 
-    if ( $_POST["confMotDePasse"] != $_POST["motDePasse"]) {
-      $motDePasseErr = "Les 2 mots de passe sont différents !"; 
-    }
-  }
-    
-  if (empty($_POST["email"])) {
-    $emailErr = "L'adresse email est requise";
-  } else {
-    $email = test_input($_POST["email"]);
-    // Vérif format email
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Adresse email invalide !"; 
-    }
-  }
-  
-  if (empty($_POST["nom"])) {
-    $nomErr = "Le nom est requis !";
-  } else {
-    $nom = test_input($_POST["nom"]);
-    // vérif si le nom ne contient que des lettres et des espaces
-    if (!preg_match("/^[a-zA-Z ]*$/",$nom)) {
-      $nomErr = "Seules les lettres et les espaces sont autorisés !"; 
-    }
-  }
+            if (isset($_POST["jeMinscris"])) {
+                if (empty($_POST["pseudo"])) {
+                    echo "Le pseudo est requis"."<br/>";
+                } else {
+                    $pseudo = test_input($_POST["pseudo"]);
+                    // vérif si le nom ne contient que des lettres et des espaces
+                if (!preg_match("/^[a-zA-Z ]*$/",$pseudo)) {
+                    $pseudoErr = "Seules les lettres et les espaces sont autorisés"."<br/>"; 
+                    }
+                }
+                if (empty($_POST["motDePasse"])) {
+                    echo "Le mot de passe est requis"."<br/>";
+                } else {
+                    $motDePasse = test_input($_POST["motDePasse"]);
+                    // vérif si le mot de passe contient majuscule, minuscule, chiffre, et caractères spéciaux
+                if (!preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W)/", $motDePasse)) {
+                    echo "Mot de passe non conforme, doit contenir une majuscule, minuscule, chiffre, et caractères spéciaux"; 
+                    }
+                }
+                
+                if (empty($_POST["confMotDePasse"])) {
+                    echo "La confirmation du mot de passe est requise"."<br>";
+                } else {
+                    $confMotDePasse= test_input($_POST["confMotDePasse"]);
+                    // vérif si mot de passe identique 
+                if ( $_POST["confMotDePasse"] != $_POST["motDePasse"]) {
+                    echo "Les 2 mots de passe sont différents !"."<br/>"; 
+                    }
+                }
+                    
+                if (empty($_POST["email"])) {
+                    echo "L'adresse email est requise"."<br/>";
+                } else {
+                    $email = test_input($_POST["email"]);
+                    // Vérif format email
+                if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                    echo "Adresse email invalide !"."<br/>"; 
+                    }
+                }
+                
+                if (empty($_POST["nom"])) {
+                    echo "Le nom est requis !"."<br/>";
+                } else {
+                    $nom = test_input($_POST["nom"]);
+                    // vérif si le nom ne contient que des lettres et des espaces
+                if (!preg_match("/^[a-zA-Z ]*$/",$nom)) {
+                    echo "Seules les lettres et les espaces sont autorisés !"."<br/>"; 
+                    }
+                }
 
-  if (empty($_POST["prenom"])) {
-    $prenomErr = "Le prenom est requis !";
-  } else {
-    $prenom = test_input($_POST["prenom"]);
-    // vérif si le prenom ne contient que des lettres et des espaces
-    if (!preg_match("/^[a-zA-Z ]*$/",$prenom)) {
-      $prenomErr = "Seules les lettres et les espaces sont autorisés !"; 
-    }
-  }
+                if (empty($_POST["prenom"])) {
+                    echo "Le prenom est requis !"."<br/>";
+                } else {
+                    $prenom = test_input($_POST["prenom"]);
+                    // vérif si le prenom ne contient que des lettres et des espaces
+                if (!preg_match("/^[a-zA-Z ]*$/",$prenom)) {
+                    echo "Seules les lettres et les espaces sont autorisés !"."<br>"; 
+                    }
+                }
 
-  if (empty($_POST["dateDeNaissance"])) {
-    $dateDeNaissanceErr = "La date de naissance est requise !";
-  } else {
-    $dateDeNaissance = test_input($_POST["dateDeNaissance"]);
-  
-  if (empty($_POST["sexe"])) {
-    $sexeErr = "Le sexe est requis";
-  } else {
-    $sexe = test_input($_POST["sexe"]);
-  }
+                if (empty($_POST["dateDeNaissance"])) {
+                    echo "La date de naissance est requise !"."<br>";
+                } else {
+                    $dateDeNaissance = test_input($_POST["dateDeNaissance"]);
+                }
+                if (empty($_POST["sexe"])) {
+                    echo "Le sexe est requis"."<br>";
+                } else {
+                    $sexe = test_input($_POST["sexe"]);
+                }
 
-  if (empty($_POST["taille"])) {
-    $tailleErr = "Votre taille est requise !";
-  } else {
-    $taille = test_input($_POST["taille"]);
-    // vérif si le champs ne contient que des chiffres
-    if (!preg_match("/^[[0-9]/",$taille)) {
-    $tailleErr = "Votre tailles en cm est requise !"; 
-    }
-  }
+                if (empty($_POST["taille"])) {
+                    echo "Votre taille est requise !"."<br>";
+                } else {
+                    $taille = test_input($_POST["taille"]);
+                    // vérif si le champs ne contient que des chiffres
+                if (!preg_match("/^[[0-9]/",$taille)) {
+                    echo "Votre tailles en cm est requise !"."<br>"; 
+                    }
+                }
 
-  if (empty($_POST["poids"])) {
-    $poidsErr = "Votre poids est requise !";
-  } else {
-    $poids = test_input($_POST["poids"]);
-    // vérif si le champs ne contient que des chiffres
-    if (!preg_match("/^[[0-9]/",$poids)) {
-    $poidsErr = "Votre poids en kg est requis !"; 
-    }
-  }
+                if (empty($_POST["poids"])) {
+                    echo  "Votre poids est requise !"."<br>";
+                } else {
+                    $poids = test_input($_POST["poids"]);
+                    // vérif si le champs ne contient que des chiffres
+                    if (!preg_match("/^[[0-9]/",$poids)) {
+                        echo "Votre poids en kg est requis !"."<br>"; 
+                    }
+                }
 
-  if (isset($_POST["activite"])){
-    $choix = $_POST["activite"];
-    if ($choix=="sedentaire"){
-      echo ("Sédentaire");
-    }
-    if ($choix=="legerementActif"){
-      echo ("Légèrement actif");
-    }
-    if ($choix=="actif"){
-      echo ("Actif");
-    }
-    if ($choix=="sportif"){
-      echo ("Sportif");
-    }
-  }
-  }
-}
+                if (isset($_POST['activite']))
+                        {
+                            switch ($_POST['activite'])
+                            {
+                                case 'Sédentaire':
+                                    echo ("Sédentaire");
+                                    break;
+                                case 'Légèrement actif':
+                                    echo ("Légèrement actif");
+                                    break;
+                                case 'Actif':
+                                    echo ("Actif");
+                                    break;
+                                case 'Sportif':
+                                    echo ("Sportif");
+                                    break;
+                                default:
+                                    echo "Veuillez renseigner votre activité" . "<br />";
+                                    break;
+                            }
+                        }
+            }
+            
 
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
-?>
-<!-- vérif récupération -->
-<?php
-    echo $pseudo;
-    echo "<br>";
-    echo $motDePasse;
-    echo "<br>";
-    echo $confMotDePasse;
-    echo "<br>";
-    echo $email;
-    echo "<br>";
-    echo $nom;
-    echo "<br>";
-    echo $prenom;
-    echo "<br>";
-    echo $dateDeNaissance;
-    echo "<br>";
-    echo $sexe;
-    echo "<br>";
-    echo $taille;
-    echo "<br>";
-    echo $poids;
-    echo "<br>";
-    echo $choix;
-?>
+            function test_input($data) {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+            }
+            ?>
+            <!-- vérif récupération -->
+            <?php
+                echo $pseudo;
+                echo "<br>";
+                echo $motDePasse;
+                echo "<br>";
+                echo $confMotDePasse;
+                echo "<br>";
+                echo $email;
+                echo "<br>";
+                echo $nom;
+                echo "<br>";
+                echo $prenom;
+                echo "<br>";
+                echo $dateDeNaissance;
+                echo "<br>";
+                echo $sexe;
+                echo "<br>";
+                echo $taille;
+                echo "<br>";
+                echo $poids;
+                echo "<br>";
+                echo $choix;
+            ?>
 
 <footer>
   <?php include("../include/footer.php")?>
