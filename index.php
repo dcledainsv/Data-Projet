a74159170d39ca1e455a51e5f4f5600040dfe3bf
Sicca <dcl.laurentj@18pixel.fr>
2019-01-18 16:29:21 +0100

    maj liens

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 
 	<body>
 		<header>
-			<img src="img/logo-bio-miam-horizon.png" alt="Titre du site et logo" class="logo-header">
+			<a href="index.php"><img src="img/logo-bio-miam-horizon.png" alt="Titre du site et logo" class="logo-header"></a>
 
 			<nav>
 				<ul>
@@ -19,7 +19,7 @@
 						<ul class="submenu">
 							<li><a href="index.php">Accueil</a></li>
 							<li><a href="">Mon Profil</a></li>
-							<li><a href="">Mes Menus</a></li>
+							<li><a href="php/menu-day.php">Mes Menus</a></li>
 							<li><a href="php/categories.php">Produits</a></li>
 							<li><a href="">Blog</a></li>
 							<li><a href="php/contact-phpmailer.php">Contact</a></li>
