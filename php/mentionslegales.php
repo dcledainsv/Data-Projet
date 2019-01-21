b5e64df5eeee5d493f932b54dadfc368ccaf216c
Sicca <dcl.laurentj@18pixel.fr>
2019-01-21 11:21:23 +0100

    maj mentions

--- php/mentionslegales.php
+++ php/mentionslegales.php
@@ -20,7 +20,7 @@
             <p>Merci de lire avec attention les différentes modalités d’utilisation du présent site avant d’y parcourir ses pages. 
             En vous connectant sur ce site, vous acceptez sans réserves les présentes modalités.
             Aussi, conformément à l’article n°6 de la Loi n°2004-575 du 21 Juin 2004 pour la confiance dans l’économie numérique, 
-            les responsables du présent site internet <a href="http://<a href="www.biomiam.fr" target="_blank">www.biomiam.fr</a>" target="_blank"><a href="www.biomiam.fr" target="_blank">www.biomiam.fr</a></a> sont :</p>
+            les responsables du présent site internet <a href="http://www.biomiam.fr" target="_blank">www.biomiam.fr</a> sont :</p>
 
             <h1>Editeur du Site :</h1>
 
